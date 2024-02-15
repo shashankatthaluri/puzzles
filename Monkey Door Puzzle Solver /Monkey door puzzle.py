6d66cb97e19977260664fd32e145b960d9790f17
Shashank. A <26676223+shashankatthaluri@users.noreply.github.com>
2024-02-15 18:50:07 +0530

    Update Monkey door puzzle.py

--- Monkey Door Puzzle Solver /Monkey door puzzle.py
+++ Monkey Door Puzzle Solver /Monkey door puzzle.py
@@ -1,1 +1,6 @@
-
+import math
+a = 1 
+b = 100 # total number of doors 
+def CountSquares(a, b):
+    return (math.floor(math.sqrt(b)) - math.ceil(math.sqrt(a)) + 1) 
+print("total number of doors open is:", int(CountSquares(a, b)))
